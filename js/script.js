5fd8fd800d8ea19b2bdf1756e43e8c76bbb10f20
ereck <ereck.c.santos@gmail.com>
2022-06-13 18:50:17 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var img = document.getElementById('imagem');
+
+
+
+function imgUM() {
+    img.style = `background-image: url('img/img1.jpg')`;
+    
+
+}
+function imgDois() {
+    img.style = `background-image: url('img/img2.jpg')`;
+
+}
+function imgTres() {
+
+    img.style = `background-image: url('img/img3.jpg')`;
+
+}
+function trocarImg() {
+    setInterval(imgDois, 5000); 
+    
+    setInterval(imgTres, 10000);
+
+    setInterval(imgUM, 15000);   
+}
+function marcarH(){
+   
+   document.getElementById('marcarHorario').style='visibility:visible;'
+   document.getElementById('body').style='overflow:hidden;'
+}
+function fecharH(){
+   
+    document.getElementById('marcarHorario').style='visibility:hidden;'
+    document.getElementById('body').style='overflow:unset;'
+}
