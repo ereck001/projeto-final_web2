f5301868b753bf4637a815768a20395be89bebb5
root <root@pop-os.localdomain>
2022-07-14 11:12:42 -0300

    banco ajustes

--- aula_QI-PHP/controller/cliente.controller.php
+++ aula_QI-PHP/controller/cliente.controller.php
@@ -6,18 +6,24 @@
    
     
     $cliente ->setNome($_POST['nome']);
-    $cliente ->setNome($_POST['idade']);
+    $cliente ->setIdade($_POST['idade']);
     $cliente ->setSexo($_POST['sexo']);
     $cliente ->setHorario($_POST['horario']);
     $cliente ->setDia($_POST['dia']);
     
+    try{
+        $strcon = mysqli_connect('localhost','php_user','N803hj&o','projphp') or die('Erro ao conectar ao banco de dados');
+        $sql = "INSERT INTO clientehora(nome,idade,sexo) VALUES ";
+        $sql .= "('$cliente ->getNome', '$cliente ->getIdade', '$cliente ->getSexo')"; 
+        mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
+        mysqli_close($strcon);
 
 
-    /*$strcon = mysqli_connect('localhost','root','N803hj&oç','projphp') or die('Erro ao conectar ao banco de dados');
-    $sql = "INSERT INTO clientehora(nome,idade,sexo) VALUES ";
-    $sql .= "('$cliente ->getNome', '$cliente ->getIdade', '$cliente ->getSexo')"; 
-    mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
-    mysqli_close($strcon);*/
+    }catch(Exception $e){ 
+        echo"erro no banco!  ".$e;
+    }
+
+    
 
     echo "<h4>Nome:{$cliente->getNome()}</h4>".
     "<h4>Idade:{$cliente->getIdade()}</h4>".
